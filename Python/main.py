00548b422e4569f3a4c021642d83936ef1784aed
Fituning <carstar@live.fr>
2021-12-21 18:10:43 +0100

    open window

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,10 @@
 import Direction
+from tkinter import *
+
+window = Tk()
+window.geometry("600x600")
+window.title("Rasend Robot")
+window.resizable(width=False, height=False)
+window.mainloop()
+
 
