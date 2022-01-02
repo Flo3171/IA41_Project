e56c73238049e0b720ee2218919b7f4884be40d5
Fituning <carstar@live.fr>
2022-01-02 17:56:03 +0100

    affichage cells

--- Python/Window.py
+++ Python/Window.py
@@ -1,21 +1,56 @@
 from tkinter import *
+from Case import Case
+from Board import Board
+from Wall import Wall
+
+_window = Tk()
+_window.title("Rasend Robot")
+canvas = Canvas(_window,bg = "black")
+canvas.place(x=0, y=0)
+img_size =38
+
+IMG_cell = PhotoImage ( file = "files/board/CaseX38.png" )
+
+def place_cell(x, y):
+        CAN_Zone_Image = canvas.create_image ( x*img_size , y*img_size , image = IMG_cell , anchor = "nw" )
+
 
 class Window:
 
-    def __init__(self, t):
-        self.window = Tk()
-        self.window.geometry(t)
-        self.window.title("Rasend Robot")
-        self.window.resizable(width=False, height=False)
+    def __init__(self, height,width):
+        _window.geometry(str(height)+"x"+str(width))
+        _window.resizable(width=False, height=False)
+        canvas.config(width=width, height=height)
+
 
     def __init__(self):
-        self.window = Tk()
-        self.window.geometry("600x600")
-        self.window.title("Rasend Robot")
-        self.window.resizable(width=False, height=False)
+        _window.geometry(str(16*img_size)+"x"+str(16*img_size))
+        _window.resizable(width=False, height=False)
+        canvas.config(width=16*img_size, height=16*img_size)
+
+
 
     def launch(self):
-        self.window.mainloop()
+        _window.mainloop()
 
-    def setsize(self,height,width):
-        self.window.geometry(str(height)+"x"+str(width))
+    def setsize(self, height, width):
+        _window.geometry(str(height)+"x"+str(width))
+
+    def place_wall(self, x, y, orient):
+        fichierimg = PhotoImage(file="files/board/Case.png")
+
+
+
+    def draw_board(self, board):
+        for i in range(16):
+            for j in range(16):
+                place_cell(i,j)
+                z = 0
+                n_wall = len(board._cases[i][j]._walls)
+                while z < n_wall:
+                    board._cases[i][j]._walls[z].dir
+                    z = z +1
+
+
+
+
--- Python/main.py
+++ Python/main.py
@@ -3,18 +3,36 @@ import Direction
 import Board
 import Game
 from Window import Window
+from tkinter import *
 
-<<<<<<< Updated upstream
-#fen = Window("600x600")
-#fen.launch()
-=======
+
 fen = Window()
-fen.launch()
->>>>>>> Stashed changes
-
-
-
 
 b = Board.Board()
 b.genreate_board()
+fen.draw_board(b)
+fen.launch()
+
+
+
+"""TKI_Principal = Tk ( )
+
+IMG_Image = PhotoImage ( file = "files/board/Case.png" )
+
+CAN_Zone = Canvas ( TKI_Principal , bg = "black" , height = 130 , width = 220 )
+CAN_Zone.grid ( row = 0 , column = 0 , sticky = "nesw" )
+CAN_Zone_Image = CAN_Zone.create_image ( 30 , 10 , image = IMG_Image , anchor = "nw" )
+
+TKI_Principal.mainloop ( )"""
+
+
+
+"""f = Tk()
+IMG_Image = PhotoImage ( file = "files/board/Case.png" )
+canvas = Canvas(f, height=500, width=500)
+canvas.grid ( row = 0 , column = 0 , sticky = "nesw" )
+CAN_Zone_Image = canvas.create_image ( 30 , 10 , image = IMG_Image , anchor = "nw" )
+f.mainloop()"""
+
+
 
